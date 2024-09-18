83b0befed37592f75c49048be8d60817f52bb42e
BiBak <Husein.samadi@gmail.com>
2024-09-18 13:15:17 +0330

    added general templates folder
    moved templates general

--- resume/views.py
+++ resume/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse ,JsonResponse
 
 def home_view(request):
-    return render(request, 'website/index.html')
+    return render(request, 'index.html')
 
 
 def about_view(request):
